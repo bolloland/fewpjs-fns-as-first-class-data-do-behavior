2a78ff6a0255d690765a73311e1e4eb6558512ee
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-05-20 22:13:20 -0500

    commit tree e6a453c7f13c4788e280628ecee75e8fd216b7d0

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let hoursMinutes = timeString.split(':');
+  const hour = parseInt(hoursMinutes[0]);
+  if (hour < 12) return "Good Morning";
+  else if (hour > 17) return "Good Evening";
+ else return "Good Afternoon";
+  }
+
 /* Write your implementation of displayMessage() */
+function displayMessage(goodMessage) {
+  document.getElementById("greeting").innerText = goodMessage;
+}
+
